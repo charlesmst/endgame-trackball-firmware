b364b20060be8465722c04ec7bc36372ac685e29
Charles Stein <charles.mst@gmail.com>
2026-06-08 22:39:34 -0300

    stepped scroll

--- scripts/build_lariska.sh
+++ scripts/build_lariska.sh
@@ -3,7 +3,8 @@ cd "$(dirname "$0")/../zmk" || exit 1
 
 CHARLESMST_PAW="$(pwd)/../../zmk-config-roBa-charybdis-esb/zmk-paw3395-driver"
 VFX_LED="$(pwd)/../../zmk-config-roBa-charybdis-esb/zmk-vfx-rgbled-indicator"
-MODULES="${CHARLESMST_PAW};${VFX_LED};$(pwd)/../lariska-config;$(pwd)/../zmk-esb-endpoint"
+STEPPED_SCROLL="$(pwd)/../zmk-behavior-stepped-scroll"
+MODULES="${CHARLESMST_PAW};${VFX_LED};$(pwd)/../lariska-config;$(pwd)/../zmk-esb-endpoint;${STEPPED_SCROLL}"
 
 west build --pristine always -s app -b nice_nano_v2 -- \
     -DSHIELD=lariska \
